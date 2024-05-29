2625080b3601aca0904b51cb3e62b4d8c4624926
ASLawan <nawalnitsua@gmail.com>
2024-05-29 13:15:24 +0100

    Rewrite given code snippet to make use of ES6 arrow functions syntax

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default function getNeighborhood() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
