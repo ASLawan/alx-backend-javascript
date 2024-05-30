7d9b3a87af2a94a1164bcd84c042cce9a73c5d34
ASLawan <nawalnitsua@gmail.com>
2024-05-30 13:21:55 +0100

    fix code output errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -9,8 +9,8 @@ export default function handleProfileSignup() {
     })
 
     .catch(() => {
-      console.log('signup system offline');
+      console.log('Signup system offline');
     });
 }
 
-//handleProfileSignup();
+// handleProfileSignup();
