c88988004ffbb29c79c77761acd1e168c05b9a7f
ASLawan <nawalnitsua@gmail.com>
2024-05-30 21:58:34 +0100

    Fix compliance errors

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
 
     queue.push(result);
   } catch (error) {
-    queue.push("Error: " + error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
