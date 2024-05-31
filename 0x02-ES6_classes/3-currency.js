da7b1a4fd47825f04fed154e39ab65f0832b5569
ASLawan <nawalnitsua@gmail.com>
2024-05-31 02:42:17 +0100

    Define a class that implements getters and setters as well as class method to display currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  // getter and setter for code attribute
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // getter and setter for name attribute
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
