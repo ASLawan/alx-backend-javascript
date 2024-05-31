28b4efc99c06a928d522afbe217584de5dccdb2e
ASLawan <nawalnitsua@gmail.com>
2024-05-31 11:23:08 +0100

    Task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
