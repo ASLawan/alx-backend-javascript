72f270cbb6c621e4b75ae2f15161cfaa816d2204
ASLawan <nawalnitsua@gmail.com>
2024-06-04 00:39:09 +0100

    Write a function that returns an array of Ids from a list of objects

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const studentIds = students.map((student) => student.id);
+
+  return studentIds;
+}
