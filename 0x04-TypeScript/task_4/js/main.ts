64c45a3b306b45779b47e36020ae10d29e334ed7
ASLawan <nawalnitsua@gmail.com>
2024-06-06 00:56:13 +0100

    Create class instances and log the examples given

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,37 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import { Subjects } from './subjects/Teacher';
+import { Subjects } from './subjects/Subject';
+import { Subjects } from './subjects/Cpp';
+import { Subjects } from './subjects/Java';
+import { Subjects } from './subjects/React';
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: "Austin",
+  lastName: "Lawan"
+  experienceTeachingC: 10,
+};
+
+// For Cpp subject
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java subject
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React subject
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
