ca13f512b2578845b4ce11cd23315499125cd487
ASLawan <nawalnitsua@gmail.com>
2024-06-05 14:59:55 +0100

    Define a Teacher interface with given properties

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+//Define a teacher interface
+
+interface Teacher {
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[key: string]: any;
+}
