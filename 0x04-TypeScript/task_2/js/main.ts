de46daeea216ac14ed1df1c44795061088e55e9a
ASLawan <nawalnitsua@gmail.com>
2024-06-05 17:13:04 +0100

    Define classes through interfaces and implement given methods

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+//Define Director interface
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+//Define Teacher interface
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+//Define Director class that implements DirectorInterface
+class Director implements DirectorInterface {
+	workFromHome(): string {
+		return "Working from Home";
+	}
+
+	getCoffeeBreak(): string {
+		return "Getting a coffee break";
+	}
+
+	workDirectorTasks(): string {
+		return "Getting to director tasks";
+	}
+}
+
+//Define Teacher class that implements TeacherInterface
+class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return "Cannot work from home";
+	}
+
+	getCoffeeBreak(): string {
+		return "Cannot have a break";
+	}
+
+	workTeacherTasks(): string {
+		return "Getting to work";
+	}
+}
+
+
+// createEmployee Function
+function createEmployee(salary: number | string): Director | Teacher {
+	if (typeof salary === 'number' && salary < 500){
+		return new Teacher();
+	} else {
+		return new Director();
+	}
+}
