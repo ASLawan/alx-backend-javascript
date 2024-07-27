29e0af5d96c38930b5a3d1848a1ced5cce52b250
ASLawan <nawalnitsua@gmail.com>
2024-07-27 15:29:28 +0100

    Fix compliance errors

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const port = 1245;
-const hostname = "localhost";
+const hostname = 'localhost';
 
-app.get("/", (req, res) => {
-	res.send("Hello Holberton School!");
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
