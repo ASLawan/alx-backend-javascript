4d8537b540cda4e75ca2c7fa23f421509a3deca7
ASLawan <nawalnitsua@gmail.com>
2024-07-27 15:22:06 +0100

    Fix code output errors

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -12,7 +12,7 @@ const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students');
+    res.write('This is the list of our students\n');
     countStudents(process.argv[2]).then((data) => {
       res.write(data);
       res.end();
