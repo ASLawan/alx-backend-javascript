8514e9e5d9221691a28c1c784ef945a48d3e1b18
ASLawan <nawalnitsua@gmail.com>
2024-07-29 09:54:06 +0100

    User 'assert'

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const assert = require('chai').assert;
+const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
