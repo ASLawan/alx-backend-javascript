ca4082f9aa1fa9852c36866f92af4da1f4b335b8
ASLawan <nawalnitsua@gmail.com>
2024-10-03 04:34:11 +0100

    Write a program executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,12 @@
 #!/usr/bin/env node
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is ${name}`);
+  process.stdin.end();
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit(0);
 });
